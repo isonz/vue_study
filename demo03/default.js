d9737c7e14c859ad7c5a89a65172e9dadaf4e296
ison <isonz@qq.com>
2019-07-18 16:35:14 +0800

    asdf

---
+++ demo03/default.js
@@ -0,0 +1,53 @@
+var app1 = new Vue({
+  el: '#app-1',
+  data: {
+    message: "",
+    checked: true,
+  }
+});
+
+var example3 = new Vue({
+  el: '#example-3',
+  data: {
+    checkedNames: []
+  }
+});
+
+var example4 = new Vue({
+  el: '#example-4',
+  data: {
+    picked: ''
+  }
+});
+
+var example5 = new Vue({
+  el: '#example-5',
+  data: {
+    selected: ''
+  }
+});
+
+var example6 = new Vue({
+  el: '#example-6',
+  data: {
+    selected: []
+  }
+});
+
+var example7 = new Vue({
+  el: '#example-7',
+  data: {
+    selected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ],
+    picked : 'b',
+    toggle: true,
+    msg: 'ssss',
+    age: 15,
+    
+  }
+});
+
